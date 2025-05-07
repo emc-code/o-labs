069ef174a13cf7ddaad240b854d160eec18c874b
Evgenii Chavkin <a110589@yandex.ru>
2025-05-07 21:10:10 +0400

    init

---
+++ Lab1-DB/CreateDB.cs
@@ -0,0 +1,62 @@
+﻿using Npgsql;
+using System.Diagnostics;
+
+namespace Lab1_DB;
+
+public static class CreateDB
+{
+    public static void Create(string connectionString)
+    {
+        using var connection = new NpgsqlConnection(connectionString);
+        connection.Open();
+
+        string sql = string.Empty;
+        sql += @"
+        CREATE TABLE Products
+        (
+            ProductId SERIAL PRIMARY KEY,
+            ProductName VARCHAR(255) NOT NULL,
+            Description TEXT,
+            Price NUMERIC(10, 2) NOT NULL,
+            QuantityInStock INT NOT NULL CHECK (QuantityInStock >= 0)
+        );
+        ";
+
+        sql += @"
+        CREATE TABLE Users
+        (
+            UserId SERIAL PRIMARY KEY,
+            UserName VARCHAR(100) NOT NULL,
+            Email VARCHAR(255) NOT NULL UNIQUE,
+            RegistrationDate DATE NOT NULL DEFAULT CURRENT_DATE
+        );
+        ";
+
+        sql += @"
+        CREATE TABLE Orders 
+        (
+            OrderId SERIAL PRIMARY KEY,
+            UserId INT NOT NULL,
+            OrderDate TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+            Status VARCHAR(50) NOT NULL,
+            FOREIGN KEY (UserId) REFERENCES Users(UserId)
+        );
+        ";
+
+        sql += @"
+        CREATE TABLE OrderDetails 
+        (
+            OrderDetailId SERIAL PRIMARY KEY,
+            OrderId INT NOT NULL,
+            ProductId INT NOT NULL,
+            Quantity INT NOT NULL CHECK (Quantity > 0),
+            TotalCost NUMERIC(10, 2) NOT NULL CHECK (TotalCost >= 0),
+            FOREIGN KEY (OrderId) REFERENCES Orders(OrderId),
+            FOREIGN KEY (ProductId) REFERENCES Products(ProductId)
+        );
+        ";
+
+        using var cmd = new NpgsqlCommand(sql, connection);
+        Debug.WriteLine(cmd.ExecuteNonQuery().ToString());
+    }
+}
---
+++ Lab1-DB/Requests.cs
@@ -0,0 +1,32 @@
+﻿using Npgsql;
+using static System.Net.Mime.MediaTypeNames;
+using System.Diagnostics;
+
+namespace Lab1_DB;
+
+public static class Requests
+{
+    public static void Run(string connectionString)
+    {
+        var conection = new NpgsqlConnection(connectionString);
+        conection.Open();
+
+        string sql = string.Empty;
+        sql += @"INSERT INTO Products (ProductName, Description, Price, QuantityInStock) VALUES ('soloma', 'rzanaya soloma', 1000, 3);";
+        sql += @"UPDATE Products SET Price = Price + 500 WHERE ProductName = 'soloma';";
+        sql += @"SELECT * FROM Orders WHERE UserId = 1;";
+        sql += @"SELECT SUM(TotalCost) FROM OrderDetails WHERE OrderId = 3;";
+        sql += @"SELECT SUM(QuantityInStock) FROM Products;";
+        sql += @"SELECT * FROM Products ORDER BY Price DESC LIMIT 5;";
+        sql += @"SELECT * FROM Products WHERE QuantityInStock < 5;";
+
+        using var cmd = new NpgsqlCommand(sql, conection);
+        using var reader = cmd.ExecuteReader();
+        while (reader.Read())
+        {
+            for (int i = 0; i < reader.FieldCount; i++)
+                Debug.Write($"{reader.GetName(i)}: {reader.GetValue(i)}\t");
+            Debug.WriteLine(" ");
+        }
+    }
+}
